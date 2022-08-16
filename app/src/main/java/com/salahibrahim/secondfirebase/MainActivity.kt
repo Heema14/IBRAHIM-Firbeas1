1f42554c100dca049a4cdf12921c939a3df226ac
Ibrahim <80535557+Heema14@users.noreply.github.com>
2022-08-16 10:36:44 +0300

    Second project firebase

--- app/src/main/java/com/salahibrahim/secondfirebase/MainActivity.kt
+++ app/src/main/java/com/salahibrahim/secondfirebase/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //
+
         supportActionBar!!.title="login"
 
         EtEmail = findViewById(R.id.Emaild)
