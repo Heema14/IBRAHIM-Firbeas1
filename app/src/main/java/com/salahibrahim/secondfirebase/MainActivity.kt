989c3088e3d160504e1b325353909e2e286826c6
Ibrahim <80535557+Heema14@users.noreply.github.com>
2022-08-16 12:51:10 +0300

    test

--- app/src/main/java/com/salahibrahim/secondfirebase/MainActivity.kt
+++ app/src/main/java/com/salahibrahim/secondfirebase/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var EtPassword: TextInputLayout
     private lateinit var btnLogin: Button
     private lateinit var btnLoginGoogle: Button
-
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
