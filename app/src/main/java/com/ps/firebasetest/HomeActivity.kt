b52e45eeb01ed6a602c5dc4917973f2ce3bf9f6c
SalahEddinAhmed <sss0592175552@gmail.com>
2022-08-16 12:55:43 +0300

    test

--- app/src/main/java/com/ps/firebasetest/HomeActivity.kt
+++ app/src/main/java/com/ps/firebasetest/HomeActivity.kt
@@ -2,13 +2,14 @@ package com.ps.firebasetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.socialMedia.firebasetest.R
+import com.salaheddin65.randomnumberfirebase.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        //
         //
 //
 
